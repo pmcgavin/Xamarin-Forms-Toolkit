f15085e61324644770d21b02cb800fcec16f8fbf
Rui Marinho <rui@mbprui.home>
2014-06-04 00:44:06 +0100

    Rename project to XForms.Toolkit
    To avoid future namespaces confusions with Xamarin it’s safer to rename
    to a distinctive name.

--- Xamarin.Forms.Toolkit/Services/ITextToSpeechService.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Xamarin.Forms.Toolkit.Services
-{
-    //http://developer.xamarin.com/guides/cross-platform/xamarin-forms/dependency-service/
-    public interface ITextToSpeechService
-    {
-        void Speak(string text);
-    }
-}
